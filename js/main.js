2f5183f1bd2ef6e5ef2689fd37c60efc06d7ecb8
Eduardas <latoanas@gmail.com>
2021-02-08 00:12:54 +0200

    Todo begin #1

---
+++ js/main.js
@@ -0,0 +1,21 @@
+import { Todo } from './components/Todo.js';
+
+const randomuzduotys = new Todo('Random uzduotis');
+
+randomuzduotys.summary();
+
+randomuzduotys.add('Sukurti pirma uzduoti');
+console.log(randomuzduotys);
+
+
+
+
+
+
+
+
+
+
+
+// const namuDarbai = new Todo('Namu darbai');
+// console.log(namuDarbai.summary());
