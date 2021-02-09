6ed46185ac359fa7a787179cc3c00b54e966cafd
Eduardas <latoanas@gmail.com>
2021-02-09 22:35:53 +0200

    Todo#2

--- js/main.js
+++ js/main.js
@@ -28,6 +28,72 @@ blynai.print();
 blynai.printCompletedOnly();
 blynai.printNotCompletedOnly();
 
+console.clear();
+
+ // arrow functions (naujasis metodas):
+
+// function suma(a, b) {
+//     return a + b; 
+// }
+// console.log(suma(4, 5));
+
+// const skirtumas = function (a, b) {
+//    return a - b;
+// }
+
+// console.log(skirtumas(4, 5));
+
+// const daugyba = (a, b) => {
+//    return a * b;
+// }
+// console.log(daugyba(4, 5));
+
+// console.log(skirtumas(4, 5));
+
+// const dalyba = (a, b) => a / b;
+// console.log(dalyba(4, 5));
+
+// const kvadratu = a => a * a;
+// console.log(kvadratu(4));
 
 
+// senasis metodas:
 
+const a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+console.log(a);
+
+const aKvadratu = [];
+for ( let i = 0; i < a.length; i++) {
+aKvadratu.push(a[i] * a[i])
+}
+console.log(aKvadratu);
+
+const bMap = a.map(n => n * n);
+console.log(bMap);
+
+console.log('-----------------------------');
+
+const lygyniai = [];
+for (let i = 0; i < a.length; i++) {
+   if (a[i] %2 === 0) {
+      lygyniai.push(a[i]);
+   }
+}
+console.log(lygyniai);
+
+const cFilter = a.filter(n => n% 2 === 0);
+console.log(cFilter);
+
+console.log('-----------------------------');
+
+const lygyniuKvadratai = [];
+for (let i = 0; i < a.length; i++) {
+   if (a[i] % 2 === 0) {
+       lygyniuKvadratai.push(a[i] * a[i]);
+   }
+}
+console.log(lygyniuKvadratai);
+
+const dMapFilter = a.filter(n => n % 2 === 0)
+      .map(n => n * n);
+      console.log(dMapFilter);
