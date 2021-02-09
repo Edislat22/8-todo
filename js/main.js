b891508463f2f82f5508869af66485b4b639d10f
Eduardas <latoanas@gmail.com>
2021-02-09 08:16:19 +0200

    Initial commit

--- js/main.js
+++ js/main.js
@@ -1,18 +1,25 @@
 import { Todo } from './components/Todo.js';
 
-const randomuzduotys = new Todo('Random uzduotis');
+const randomuzduotys = new Todo('Random uzduotys');
+const namuDarbai = new Todo('Namu darbai');
 
 randomuzduotys.summary();
 
 randomuzduotys.add('Sukurti pirma uzduoti');
+namuDarbai.add('Susitvarkyti Serloko projekta');
+namuDarbai.add('Susitvarkyti Serloko projekta');
+namuDarbai.add('Susitvarkyti Serloko projekta');
+randomuzduotys.add('Uzmaisyti tesla');
+namuDarbai.add('Susitvarkyti Serloko projekta');
+randomuzduotys.add('Nusiskusti');
+namuDarbai.add('Susitvarkyti Serloko projekta');
+namuDarbai.add('Susitvarkyti Serloko projekta');
+
 console.log(randomuzduotys);
+console.log(namuDarbai);
 
-
-
-
-
-
-
+randomuzduotys.summary();
+namuDarbai.summary();
 
 
 
